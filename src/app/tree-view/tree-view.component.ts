b26300432d381c3bdfb73f35014728b6efc143b7
PriyaGupta <pgup9374@gmail.com>
2025-04-17 15:29:47 +0530

    Added changes for tree view element

--- src/app/tree-view/tree-view.component.ts
+++ src/app/tree-view/tree-view.component.ts
@@ -1,110 +1,96 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import {MatTreeFlatDataSource, MatTreeFlattener, MatTreeModule} from '@angular/material/tree';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import { FlatTreeControl } from '@angular/cdk/tree';
-
-
-
-
-/** Flat node with expandable and level information */
-interface ExampleFlatNode {
-  expandable: boolean;
-  name: string;
-  level: number;
-  role: string; 
-}
-
-interface BusinessNode {
-  name: string;
-  role: string;
-  children?: BusinessNode[];
-}
-
-const BUSINESS_TREE_DATA: BusinessNode[] = [
-  {
-    name: "John Doe",
-    role: "Company Owner",
-    children: [
-      {
-        name: "Sarah Smith",
-        role: "CEO",
-        children: [
-          {
-            name: "David Johnson",
-            role: "Director",
-            children: [
-              { name: "Emma Wilson", role: "VP" },
-              { name: "James Brown", role: "VP" }
-            ]
-          },
-          {
-            name: "Michael Clark",
-            role: "Director",
-            children: [
-              { name: "Sophia Lewis", role: "VP" }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-];
-
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { TreeNode } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-tree-view',
-  imports: [MatTreeModule, MatButtonModule, MatIconModule],
+  imports: [OrganizationChartModule,ButtonModule],
+  standalone:true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './tree-view.component.html',
   styleUrl: './tree-view.component.css'
 })
 export class TreeViewComponent {
-  private _transformer = (node: BusinessNode, level: number): ExampleFlatNode & { role: string } => {
-    return {
-      expandable: !!node.children && node.children.length > 0,
-      name: node.name,
-      level: level,
-      role: node.role // 👈 include role
-    };
-  };
-  
+  data1: any = null;
+  selectedNodes!: TreeNode[];
 
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level,
-    node => node.expandable,
-  );
 
-  treeFlattener = new MatTreeFlattener(
-    this._transformer,
-    node => node.level,
-    node => node.expandable,
-    node => node.children,
-    
-  );
+  data: TreeNode[] = [
+      {
+          expanded: true,
+          type: 'person',
+          data: {
+              image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/amyelsner.png',
+              name: 'Amy Elsner',
+              title: 'CEO'
+          },
+          children: [
+              {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                      image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/annafali.png',
+                      name: 'Anna Fali',
+                      title: 'CMO'
+                  },
+                  children: [
+                    {
+                        expanded: true,
+                        type: 'person',
+                        data: {
+                            image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/annafali.png',
+                            name: 'RMO Singh',
+                            title: 'Director'
+                        },
+                        children: [
+                            {
+                                label: 'Research'
+                            },
+                            {
+                                label: 'Marketing'
+                            }
+                        ]
+                    },
+                      {
+                          label: 'Marketing'
+                      }
+                  ]
+              },
+              {
+                  expanded: true,
+                  type: 'person',
+                  data: {
+                      image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/stephenshaw.png',
+                      name: 'Stephen Shaw',
+                      title: 'CTO'
+                  },
+                  children: [
+                      {
+                          label: 'Development'
+                      },
+                      {
+                          label: 'UI/UX Design'
+                      }
+                  ]
+              },
 
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+              {
+                expanded: true,
+                type: 'person',
+                data: {
+                    image: 'https://primefaces.org/cdn/primeng/images/demo/avatar/stephenshaw.png',
+                    name: 'Stencil Shaw',
+                    title: 'CTO',
 
-  constructor() {
-    this.dataSource.data = BUSINESS_TREE_DATA;
-  }
+                }
+            }
+          ]
+      }
+  ];
 
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
-  getIcon(role: string): string {
-    switch (role) {
-      case "Company Owner":
-        return "business";
-      case "CEO":
-        return "star"; 
-      case "Director":
-        return "supervisor_account"; 
-      case "VP":
-        return "person";
-      default:
-        return "account_circle";
-    }
-  }
   
 }
+
 
 
